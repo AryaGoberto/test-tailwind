3bae5d636e11a5b778b9ad5da07893d92b548c64
AryaGoberto <arguna2019@gmail.com>
2024-08-08 22:18:29 +0800

    tambah darkmode dan update fitur tambahan

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["index.html"],
+  darkMode: "class",
   theme: {
     container:{
       center: true,
@@ -11,7 +12,8 @@ module.exports = {
         primary: '#512D6D',
         bg: "#EEEEEE",
         secondary: "#64748b",
-        font: "#0f172a"
+        font: "#0f172a",
+        dark: "#1e293b",
       },
       screens:{
         '2xl': '1320px'
